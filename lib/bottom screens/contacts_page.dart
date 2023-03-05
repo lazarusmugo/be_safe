f5a4cc2b530bacb5b99147e1bd53c3f347e04ca8
lazarusmugo <lazzohchainz@gmail.com>
2023-03-05 21:13:20 +0300

    added group map page, changed contact page

--- lib/bottom screens/contacts_page.dart
+++ lib/bottom screens/contacts_page.dart
@@ -1,36 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:contacts_service/contacts_service.dart';
 
 class ContactsPage extends StatefulWidget {
-  const ContactsPage({Key? key}) : super(key: key);
+  const ContactsPage({super.key});
 
   @override
-  _ContactsPageState createState() => _ContactsPageState();
+  State<ContactsPage> createState() => _ContactsPageState();
 }
 
 class _ContactsPageState extends State<ContactsPage> {
-  dynamic _contacts = {};
-
-  @override
-  void initState() {
-    getContacts();
-    super.initState();
-  }
-
-  Future<void> getContacts() async {
-    if (mounted) {
-      Iterable<Contact>? contacts =
-          await ContactsService.getContacts(withThumbnails: false);
-      setState(() {
-        _contacts = contacts;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text(_contacts.toString())),
+    return const Scaffold(
+      body: Center(child: Text('Contacts')),
     );
   }
 }
